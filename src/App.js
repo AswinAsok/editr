88a0e202e31882ab8b734b8b1960b64de66122f7
Aswin Asok <aswinasokofficial@gmail.com>
2022-03-15 15:23:42 +0530

    Layouting

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Navbar from "./Components/Navbar/Navbar.jsx";
 import Editor from "./Components/Editor/Editor.jsx";
 import Options from "./Components/Options/Options.jsx";
 import Details from "./Components/Details/Details";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Editor />
-      <Details />
+      <div className="main-container">
+        <Editor />
+        <Details />
+      </div>
       {/* <Options /> */}
     </div>
   );
