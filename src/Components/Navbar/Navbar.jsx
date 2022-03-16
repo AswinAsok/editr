e39b7661db23527dae8db74d8104cede3653f06c
Aswin Asok <aswinasokofficial@gmail.com>
2022-03-16 16:30:28 +0530

    Navbar buttons are added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-        <div className="navbar-container">
-            <p className="projname">Text.ly</p>
-            <p className="tagline">The Minimal Text Editor</p>
+      <div className="navbar-container">
+        <div className="proj-branding">
+          <p className="projname">Text.ly</p>
+          <p className="tagline">The Minimal Text Editor</p>
         </div>
+        <div className="nav-btns">
+          <button className="nbtn">Source Code</button>
+          <button className="nbtn">Follow Me</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
